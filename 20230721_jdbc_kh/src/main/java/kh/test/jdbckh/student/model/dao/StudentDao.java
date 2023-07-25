9f7dab2f28eca28e7673b748aa193d5d3dd12923
Solbeen0531 <netps0201@gmail.com>
2023-07-25 12:33:06 +0900

    search student page
    - search servlet
    - page 이동
    - 검색 결과 없을 때, '검색 결과 없음' 텍스트 표시
    - 검색 결과 없을 때, '전체보기'로 전체 학생 리스트 표시

--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/model/dao/StudentDao.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/model/dao/StudentDao.java
@@ -23,10 +23,8 @@ public class StudentDao {
 //		String query = "select * from tb_student "
 //				+ "join tb_department using(department_no)"
 //				+ "where student_no = "+"'"+studentNo+"'";
-		String query = "select * from tb_student "
-				+ "join tb_department using(department_no)"
-				+ "where student_no = ?'";
-		//위치홀더
+		String query = "select * from tb_student " + "join tb_department using(department_no)" + "where student_no = ?";
+		// 위치홀더
 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -42,14 +40,12 @@ public class StudentDao {
 //				System.out.println("연결성공");
 //			}
 			pstmt = conn.prepareStatement(query);
-			// pstmt 생성된 후 ----  execute 실행하기 전
+			// pstmt 생성된 후 ---- execute 실행하기 전
 			// 여기서 ? 위치홀더에 값 넣기
-			pstmt.setString(0, query);
-			
-			
-			
+			pstmt.setString(1, studentNo);
+
 			rset = pstmt.executeQuery();
-			if (rset.next())   {
+			if (rset.next()) {
 				result = new StudentVo();
 				// while문 동작필요없음. query결과가 단일행이기 때문임
 				result.setAbsenceYn(rset.getString("Absence_Yn"));
@@ -60,12 +56,10 @@ public class StudentDao {
 				result.setStudentName(rset.getString("Student_Name"));
 				result.setStudentNo(rset.getString("Student_No"));
 				result.setStudentSsn(rset.getString("Student_Ssn"));
-				
+
 				result.setDepartmentName(rset.getString("Department_Name"));
-				
+
 			}
-			
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -85,12 +79,13 @@ public class StudentDao {
 		return result;
 	}
 
+	// DB에서 tb_student 테이블의 있는 모든 내용을 읽어서 꺼냄
 	public List<StudentVo> selectListStudent() {
-		// DB에서 tb_student 테이블의 있는 모든 내용을 읽어서 꺼냄
 		List<StudentVo> result = null;
 		Connection conn = null;
 		Statement stmt = null;
 		PreparedStatement pstmt = null;
+		String query = "select * from tb_student";
 
 		try {
 			// 1. driver가 있다면 로딩함. 없다면 Class Not Found에러
@@ -108,8 +103,10 @@ public class StudentDao {
 			// 3. Statement/PreparedStatement 객체 생성 - conn 객체로부터 - query 문을 실어보냄
 //			stmt = conn.createStatement();
 //			pstmt = conn.prepareStatement("select * from tb_student");
-			String query = "select * from tb_student";
 			pstmt = conn.prepareStatement(query);
+
+			// 3-4 사이 위치폴더 ? 에 값 설정
+//			pstmt.setString(1, searchWord);
 
 			// 4. query문을 실행해달라고 함 - 그 결과값을 return 받음
 			// select query문이면 ResultSet모양
@@ -159,6 +156,94 @@ public class StudentDao {
 			}
 		}
 //		System.out.println(result);
+		return result;
+	}
+
+	// DB에서 tb_student 테이블의 있는 모든 내용을 읽어서 꺼냄
+	public List<StudentVo> selectListStudent(String searchWord) {
+		List<StudentVo> result = null;
+//				String query = "select * from tb_student";
+		// 오류String query= "select * from tb_student where student_name like '%?%' or
+		// student_address like '%?%'";
+		String query = "select * from tb_student where student_name like ? or student_address like ?";
+
+		Connection conn = null;
+		Statement stmt = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			// 1. driver가 있다면 로딩함. 없다면 Class Not Found에러
+//				Class.forName("oracle.jdbc.driver.OracleDriver"); // Class.forName을 기억 / try~catch
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 객체 생성 // dbms와 연결
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "kh", "kh");
+//				if (conn != null) {
+//					System.out.println("DB연결 성공!!!!!!!!!!!!");
+//				} else {
+//					System.out.println("--------------DB 연결 실패-------");
+//				}
+
+			// 3. Statement/PreparedStatement 객체 생성 - conn 객체로부터 - query 문을 실어보냄
+//				stmt = conn.createStatement();
+//				pstmt = conn.prepareStatement("select * from tb_student");
+			// like 연산자인 경우 위치홀더? 대신 연결자
+
+			pstmt = conn.prepareStatement(query);
+
+			// 3-4 사이 위치폴더 ? 에 값 설정
+			searchWord = "%" + searchWord + "%"; // like 연산자인 경우 % 또는 _ 를 합쳐서 만듬
+			pstmt.setString(1, searchWord);
+			pstmt.setString(2, searchWord);
+
+			// 4. query문을 실행해달라고 함 - 그 결과값을 return 받음
+			// select query문이면 ResultSet모양
+			// insert/update/delete문이면 int 모양
+			ResultSet rs = pstmt.executeQuery();
+
+			// 5. ResultSet에서 row(record)=한줄 읽어오기 위해 cursor(포인트)를 이동함
+			result = new ArrayList<StudentVo>();
+			while (rs.next() == true) {
+				// 한줄 row/record를 읽을 준비 완료
+//					System.out.println(rs.getString("STUDENT_NAME"));
+				StudentVo vo = new StudentVo();
+
+//					vo.setStudentName(rs.getString("student_name"));
+				vo.setDepartmentNo(rs.getString("department_no"));
+				vo.setStudentName(rs.getString("Student_Name"));
+				vo.setAbsenceYn(rs.getString("absence_Yn"));
+				vo.setCoachProfessorNo(rs.getString("coach_Professor_No"));
+				vo.setStudentAddress(rs.getString("student_Address"));
+				vo.setEntranceDate(rs.getDate("entrance_Date"));
+				vo.setStudentSsn(rs.getString("student_Ssn"));
+				vo.setStudentNo(rs.getString("student_No"));
+
+				result.add(vo);
+
+			}
+
+		} catch (ClassNotFoundException e) {
+			// 1. driver (ojdbc.jar) 없음
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// 2. dbms에 connection 실패
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+//			System.out.println(result);
 		return result;
 	}
 
--- 20230721_jdbc_kh/src/main/webapp/WEB-INF/view/student/list.jsp
+++ 20230721_jdbc_kh/src/main/webapp/WEB-INF/view/student/list.jsp
@@ -10,37 +10,64 @@
 </head>
 <body>
 	<h2>학생 목록</h2>
+
+	<div>
+		<form action="<%=request.getContextPath()%>/student/list" method="get">
+			<input type="search" name="searchWord"> <input type="submit"
+				value="찾기">
+		</form>
+	</div>
+
+
+
+
 	<%
 	// JSP Tag - java문법
-	/* String a = (String) request.getAttribute("aaa");
-	String b = (String) request.getAttribute("bbb");
-	int c = (Integer) request.getAttribute("ccc"); */
-	List<StudentVo> volist = (List<StudentVo>) request.getAttribute("studentList");
-	%>
-	<%-- <%=a %> --%>
-	<%-- <%=b %> --%>
-	<%-- <%=c %> --%>
-	<%-- <%=volist %> --%>
 
+	List<StudentVo> volist = (List<StudentVo>) request.getAttribute("studentList");
+	String searchWord = (String) request.getAttribute("searchWord");
+	if (searchWord != null) {
+	%>
+	<%-- <%=volist %> --%>
+	<h3><%=searchWord%>검색 결과
+	</h3>
+	<%
+	}
+	if (volist == null || volist.size() == 0) {
+	%>
+	<h5>
+		<a href="<%=request.getContextPath()%>/student/list">전체보기</a>
+	</h5>
+	<h2>결과물이 없습니다.</h2>
+	<%
+	} else {
+	%>
 	<table border="1">
+		
 		<tr>
 			<td>학번</td>
 			<td>이름</td>
 			<td>입학일</td>
+			<td>주소</td>
 		</tr>
 		<%
 		for (int i = 0; i < volist.size(); i++) {
 			StudentVo vo = volist.get(i);
 		%>
 		<tr>
-			<td><a href="<%=request.getContextPath()%>/student/get?sno=<%=vo.getStudentNo()%>"><%=vo.getStudentNo()%></a></td>
+			<td><a
+				href="<%=request.getContextPath()%>/student/get?sno=<%=vo.getStudentNo()%>"><%=vo.getStudentNo()%></a></td>
 			<td><%=vo.getStudentName()%></td>
 			<td><%=vo.getEntranceDate()%></td>
+			<td><%=vo.getStudentAddress()%></td>
 		</tr>
 
 		<%
 		}
 		%>
 	</table>
+	<%
+	}
+	%>
 </body>
 </html>
